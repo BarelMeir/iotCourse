43571f00006ea848c27f280ef3186f64f195d0d5
BarelMeir <barel.meir@post.idc.ac.il>
2018-11-26 20:19:40 +0200

    tt

--- simonSense.py
+++ simonSense.py
@@ -113,7 +113,6 @@ while True:
 		ledAndSound(0)
 		sleep(1)
 		checkUserInput(0)
-
 	#elif(potentiometerValue )
 
 
@@ -125,7 +124,7 @@ while True:
  #        # The read_adc function will get the value of the specified channel (0-7).
  #        values[i] = mcp.read_adc(i)
  #    # Print the ADC values.
-     print('| {0:>4} | {1:>4} | {2:>4}'.format(potentiometerValue, fireSensorValue,distanceSensor))
+    print('| {0:>4} | {1:>4} | {2:>4}'.format(potentiometerValue, fireSensorValue,distanceSensor))
 
 	
 	#print ("accel_Data ", accel_data)
