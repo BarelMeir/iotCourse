e4c4018a700d2a16188b6ad139d941dca1cb3e97
Nadav Miran <nmiran@Nadavs-MacBook.local>
2019-01-07 18:09:03 +0200

    ...

--- change_shadow.py
+++ change_shadow.py
@@ -1,10 +1,11 @@
 import boto3
+import json
 
 client = boto3.client('iot-data')
 
 response = client.update_thing_shadow(
-	thingName = 'Meir-Miran-Raspi'
-	payload= json.dumps({"desired":{interval : 3}})
+	thingName = 'Meir-Miran'
+	payload= json.dumps({"state": {"desired":{interval : 3}}})
 )
 
 print (response)
